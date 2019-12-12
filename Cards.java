1e5470a1e6d0970bc7e5703dc1de052c19e89430
Dan Ochiana <dan.mihai.ochiana@gmail.com>
2019-12-12 16:48:12 +0200

    formating

--- Cards.java
+++ Cards.java
@@ -1,17 +1,19 @@
 //Miruna Andreea Lazar 2459499L
 
-public class Cards{
-	//String methods to generate three cards
-	public static String getCard(){
-		//generates a random number within the 5 elements of the array range
-        int randomCard = (int) (Math.random()*5);
-        //
-        String cards = new String [] {"King","Queen","Jack","Ace","Joker"}[randomCard];
-        return cards;
-    }
+public class Cards {
+	// String methods to generate three cards
+	public static String getCard() {
+		// generates a random number within the 5 elements of the array range
+		int randomCard = (int) (Math.random() * 5);
+		//
+		String cards = new String[] { "King", "Queen", "Jack", "Ace", "Joker" }[randomCard];
+		return cards;
+	}
+
 	public String toString() {
 		return getCard();
 	}
 }
 
-//Reference: https://stackoverflow.com/questions/6726963/random-string-from-string-array-list
+// Reference:
+// https://stackoverflow.com/questions/6726963/random-string-from-string-array-list
--- MainFrame.java
+++ MainFrame.java
@@ -1,8 +1,10 @@
+
 //Miruna Andreea Lazar 2459499L
 import java.awt.*;
 import javax.swing.*;
+
 public class MainFrame extends JFrame {
-    
+
 	private JLabel welcome;
 	private JLabel balance;
 	private JButton spin;
@@ -13,65 +15,65 @@ public class MainFrame extends JFrame {
 	private JPanel cardOnePanel;
 	private JPanel cardTwoPanel;
 	private JPanel cardThreePanel;
-	
+
 	public MainFrame() {
-    	 super("Fruit Machine");
-    	 
-    	 setLayout(new GridLayout(7,5,5,5));
-    	 
-    	 welcome = new JLabel("Welcome!"); //create Welcome label
-    	 
-    	 balance = new JLabel("Balance is"); //create Balance is label
-    	
-    	 spin = new JButton("Spin"); //create spin button
-    	 spin.setSize(50, 50);
-    
-    	 newGame = new JButton("New Game"); //create new game button
-    	 newGame.setSize(50, 50);
-    	 
-    	 //create label of cards
-    	 cardOne = new JLabel("King");
-    	 cardTwo = new JLabel("Queen");
-    	 cardThree = new JLabel("Jack");
-    	 
-    	//create "cards" as panes, to be able to set colours and border
-    	 cardOnePanel = new JPanel();
-    	 cardTwoPanel = new JPanel();
-    	 cardThreePanel = new JPanel();
-    	 
-    	 //set the size of "cards"
-    	 cardOnePanel.setPreferredSize(new Dimension(50, 50));
-    	 cardTwoPanel.setPreferredSize(new Dimension(50, 50));
-    	 cardTwoPanel.setPreferredSize(new Dimension(50, 50));
-    	 
-    	 //add labels on the "cards"
-    	 cardOnePanel.add(cardOne);
-    	 cardTwoPanel.add(cardTwo);
-    	 cardThreePanel.add(cardThree);
-    	 
-    	 //set colours and borders of the three panels
-    	 cardOnePanel.setBackground(Color.YELLOW);
-    	 cardOnePanel.setBorder(BorderFactory.createLineBorder(null, 1));
-    	 cardTwoPanel.setBackground(Color.YELLOW);
-    	 cardTwoPanel.setBorder(BorderFactory.createLineBorder(null, 1));
-    	 cardThreePanel.setBackground(Color.YELLOW);
-    	 cardThreePanel.setBorder(BorderFactory.createLineBorder(null, 1));
-    	 
-    	 //layout of the "cards", buttons and labels
-    	 setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-    	 add(balance);
-    	 add(welcome);
-       	 add(cardOnePanel);
-    	 add(cardTwoPanel);
-    	 add(cardThreePanel);
-    	 add(spin);
-    	 add(newGame);
-    	 
-    	 //size of the frame and misc
-    	 setSize(600,400);
-    	 setLocation(550,350);
-	     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	     setVisible(true);	
-     }
-	
+		super("Fruit Machine");
+
+		setLayout(new GridLayout(7, 5, 5, 5));
+
+		welcome = new JLabel("Welcome!"); // create Welcome label
+
+		balance = new JLabel("Balance is"); // create Balance is label
+
+		spin = new JButton("Spin"); // create spin button
+		spin.setSize(50, 50);
+
+		newGame = new JButton("New Game"); // create new game button
+		newGame.setSize(50, 50);
+
+		// create label of cards
+		cardOne = new JLabel("King");
+		cardTwo = new JLabel("Queen");
+		cardThree = new JLabel("Jack");
+
+		// create "cards" as panes, to be able to set colours and border
+		cardOnePanel = new JPanel();
+		cardTwoPanel = new JPanel();
+		cardThreePanel = new JPanel();
+
+		// set the size of "cards"
+		cardOnePanel.setPreferredSize(new Dimension(50, 50));
+		cardTwoPanel.setPreferredSize(new Dimension(50, 50));
+		cardTwoPanel.setPreferredSize(new Dimension(50, 50));
+
+		// add labels on the "cards"
+		cardOnePanel.add(cardOne);
+		cardTwoPanel.add(cardTwo);
+		cardThreePanel.add(cardThree);
+
+		// set colours and borders of the three panels
+		cardOnePanel.setBackground(Color.YELLOW);
+		cardOnePanel.setBorder(BorderFactory.createLineBorder(null, 1));
+		cardTwoPanel.setBackground(Color.YELLOW);
+		cardTwoPanel.setBorder(BorderFactory.createLineBorder(null, 1));
+		cardThreePanel.setBackground(Color.YELLOW);
+		cardThreePanel.setBorder(BorderFactory.createLineBorder(null, 1));
+
+		// layout of the "cards", buttons and labels
+		setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		add(balance);
+		add(welcome);
+		add(cardOnePanel);
+		add(cardTwoPanel);
+		add(cardThreePanel);
+		add(spin);
+		add(newGame);
+
+		// size of the frame and misc
+		setSize(600, 400);
+		setLocation(550, 350);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+
 }
